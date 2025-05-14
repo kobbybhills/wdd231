19f7827d1a79d801e3897707d99875c73d5c96f4
Douglas Justice <Kobbybhills462@gmail.com>
2025-05-14 19:19:38 +0000

    Started rewriting Wk1

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,9 +1,8 @@
 // Toggle mobile menu
 document.getElementById("menuButton").addEventListener("click", () => {
-    document.getElementById("navMenu").classList.toggle("open");
-  });
-  
-  // Display last modified date
-  document.getElementById("lastModified").textContent =
-    "Last Update: " + document.lastModified;
-  
+  document.getElementById("navMenu").classList.toggle("open");
+});
+
+// Display last modified date
+document.getElementById("lastModified").textContent =
+  "Last Modified: " + document.lastModified;
