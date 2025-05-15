ba970388006a5af1e5844afefe753ce4466c644d
Douglas Justice <Kobbybhills462@gmail.com>
2025-05-15 00:33:23 +0000

    Done 4

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,11 +1,8 @@
+
 // Toggle mobile menu
 const menuButton = document.getElementById('menuButton');
-const nav = document.querySelector('nav');
+const nav = document.getElementById('mainNav');
 
 menuButton.addEventListener('click', () => {
   nav.classList.toggle('show');
 });
-
-
-
-
