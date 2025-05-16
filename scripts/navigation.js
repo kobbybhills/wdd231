0a7873012bd897b28bc4d18bc451818addb63383
Douglas Justice <Kobbybhills462@gmail.com>
2025-05-16 23:12:28 +0000

    Done 7

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,8 +1,8 @@
 // navigation.js
-
 const menuButton = document.getElementById('menuButton');
 const nav = document.getElementById('mainNav');
 
 menuButton.addEventListener('click', () => {
   nav.classList.toggle('show');
 });
+
